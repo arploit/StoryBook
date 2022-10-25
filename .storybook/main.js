e760e981fd16fc1afc008f98db093935c5ff5b3d
arploit <arpesh_11@hotmail.com>
2022-10-26 04:15:38 +0530

    Less has started working added assest file for color

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,7 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
-		{
-			name: '@storybook/preset-create-react-app',
-			options: {
-				craOverrides: {
-					fileLoaderExcludes: ['less'],
-				},
-			},
-		},
+		'storybook-preset-less',
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-interactions',
