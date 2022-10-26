92abc98247dee14720ed706a8d295e44e42b8e86
arploit <arpesh_11@hotmail.com>
2022-10-26 22:54:06 +0530

    Color swapping is working used:Inline Style

--- src/components/Swatches/Swatches.tsx
+++ src/components/Swatches/Swatches.tsx
@@ -28,6 +28,19 @@ const swatcheColor = (variants: string): Array<string> => {
     }
 }
 
+const swatchBackgroundColor = (variants: string): Array<string> => {
+    switch (variants) {
+        case 'blue': return BlueColor
+        case 'pink': return PinkColor
+        case 'green': return GreenColor
+        case 'yellow': return YellowColor
+        case 'orange': return OrangeColor
+        case 'red': return RedColor
+        case 'purple': return PurpleColor
+        case 'Gray': return GrayColor
+    }
+}
+
 const Swatches = ({ variants = 'blue' }: IProps) => {
 
 
@@ -36,15 +49,17 @@ const Swatches = ({ variants = 'blue' }: IProps) => {
             <div className='swatches__Block'>
                 {swatcheColor(variants).map((item, idx) => {
                     return (
-                        <div key={idx} className={`swatches __${variants}_${idx + 1}`}>
-                            <span>
+                        <div key={idx} className={`swatches`} style={{ background: item }}>
+                            <span style={{
+                                color: idx < 9 ? "#fff" : "#000"
+                            }}>
                                 {item}
                             </span>
                         </div>
                     )
                 })}
             </div>
-        </div>
+        </div >
     )
 }
 
